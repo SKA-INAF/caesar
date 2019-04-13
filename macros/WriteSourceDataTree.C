e5207be4b30dc59d552f5fb5ab0a24368d734b74
riggi <simone.riggi@gmail.com>
2019-04-13 11:40:28 +0200

    Add ROOT macro to write summary TTRee from Source collection

---
+++ macros/WriteSourceDataTree.C
@@ -0,0 +1,52 @@
+
+#include <Source.h>
+#include <SourceExporter.h>
+#include <iostream>
+
+using namespace std;
+using namespace Caesar;
+
+
+void WriteSourceDataTrees(std::string filename="",std::string outfilename="sourceDataTree.root",std::string outfilename_comp="sourceCompDataTree.root")
+{
+	//## Read input file
+	TFile* inputFile= new TFile(filename.c_str(),"READ");
+	if(!inputFile || inputFile->IsZombie()) {
+		cerr<<"ERROR: Cannot open file "<<filename<<"!"<<endl;
+		exit(1);
+	}
+	
+	//## Get source tree
+	TTree* SourceInfo= (TTree*)inputFile->Get("SourceInfo");	
+	if(!SourceInfo){
+		cerr<<"ERROR: Cannot get sources from file "<<filename<<"!"<<endl;
+		exit(1);
+	}
+
+	Source* aSource= 0;
+	SourceInfo->SetBranchAddress("Source",&aSource);
+
+	cout<<"# "<<SourceInfo->GetEntries()<<" sources to be drawn..."<<endl;
+	
+	//## Store source list 
+	std::vector<Source*> sources;
+	
+	for(int i=0;i<SourceInfo->GetEntries();i++){
+		SourceInfo->GetEntry(i);
+
+		Source* source= new Source;
+		*source= *aSource;
+		sources.push_back(source);
+
+	}//end loop sources
+
+	//## Write source collection to simple TTree
+	cout<<"INFO: Writing source data TTree..."<<endl;
+	SourceExporter::WriteToROOT(outfilename,sources);
+
+	cout<<"INFO: Writing source component data TTree..."<<endl;
+	SourceExporter::WriteComponentsToROOT(outfilename_comp,sources);
+
+}//close macro
+
+
