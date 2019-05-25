3d7adf8f73a011fba53e397ec6161c874ea56b76
riggi <simone.riggi@gmail.com>
2019-05-25 15:16:05 +0200

    Add print

--- scripts/cnn_training.py
+++ scripts/cnn_training.py
@@ -620,7 +620,7 @@ class CNNTrainer(object):
 		xmax= int(x0+dx/2)
 		ymin= int(y0-dy/2)
 		ymax= int(y0+dy/2)		
-		crop_data= self.img_data[ymin:ymax+1,xmin:xmax+1]
+		crop_data= self.img_data[ymin:ymax,xmin:xmax]
 	
 		#- Replace NAN with zeros and inf with large numbers
 		np.nan_to_num(crop_data,False)
